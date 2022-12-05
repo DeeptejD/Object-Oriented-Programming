bc1796aaf53f661383fb481e889e6175f33f5b65
DeeptejD <deeptejdhauskar2003@gmail.com>
2022-12-05 23:28:18 +0530

    Changes to be committed: 	new file:   Deeptej/Experiment #9/9B_generic_vector.cpp

---
+++ Deeptej/Experiment #9/9B_generic_vector.cpp
@@ -0,0 +1,11 @@
+// Write a C++ program to create a class template to represent a generic vector.Include the
+// member functions to perform the following tasks 1. Create the vector 2. To modify the value of a given
+// element 3. To display the vector elements
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
