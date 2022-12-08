3f9fa8aa79525556f8f142febb5be919940c9959
DeeptejD <deeptejdhauskar2003@gmail.com>
2022-12-09 01:17:11 +0530

    Changes to be committed: 	modified:   Deeptej/Experiment #9/9B_generic_vector.cpp

--- Deeptej/Experiment #9/9B_generic_vector.cpp
+++ Deeptej/Experiment #9/9B_generic_vector.cpp
@@ -22,13 +22,88 @@ public:
             cin >> ptr[i];
         }
     }
-    void modify(T x)
+    void modify()
     {
+        T x, y;
+        cout << "Enter the element to modify: ";
+        cin >> x;
+        cout << "Enter new element: ";
+        cin >> y;
+        for (int i = 0; i < size; i++)
+        {
+            if (ptr[i] == x)
+            {
+                ptr[i] = y;
+                cout << "Element succesfully modified\n";
+                return;
+            }
+        }
+        cout << "Element not present in the vector\n";
+    }
+    void display()
+    {
+        cout << "[ ";
+        for (int i = 0; i < size; i++)
+            cout << ptr[i] << ", ";
+        cout << "]\n";
     }
 };
 
+void intvector()
+{
+    cout << "Enter size: ";
+    int n;
+    cin >> n;
+    Vector<int> v1(n);
+    v1.display();
+    v1.modify();
+    v1.display();
+}
+
+void charvector()
+{
+    cout << "Enter size: ";
+    int n;
+    cin >> n;
+    Vector<char> v1(n);
+    v1.display();
+    v1.modify();
+    v1.display();
+}
+
+void flvector()
+{
+    cout << "Enter size: ";
+    int n;
+    cin >> n;
+    Vector<float> v1(n);
+    v1.display();
+    v1.modify();
+    v1.display();
+}
+
 int main(int argc, char const *argv[])
 {
+    int choice;
+    while (1)
+    {
+        cout << "Enter choice\n1. int Vector\n2. char Vector\n3. float Vector\n4. Exit\n";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            intvector();
+            break;
+        case 2:
+            charvector();
+            break;
+        case 3:
+            flvector();
+            break;
+        case 4:
+            exit(1);
+        }
+    }
 
     return 0;
 }
