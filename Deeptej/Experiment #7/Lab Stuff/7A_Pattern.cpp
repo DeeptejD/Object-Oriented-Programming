686f155a5b2eb83affc3be2b4930e3b6521f76f3
DeeptejD <deeptejdhauskar2003@gmail.com>
2023-02-14 23:51:44 +0530

    ...

--- Deeptej/Experiment #7/Lab Stuff/7A_Pattern.cpp
+++ Deeptej/Experiment #7/Lab Stuff/7A_Pattern.cpp
@@ -3,14 +3,12 @@
 
 using namespace std;
 
-
-
 void secondhalf(char *str, string s, int len)
 {
     for (int j = 2; j <= len; j++)
     {
         cout.fill(' ');
-        cout.width(len);
+        // cout.width(len);
         cout.write(str, j);
         cout.width(len - j);
         cout << "";
@@ -26,8 +24,8 @@ void print_pattern(char *str)
     int len = strlen(str);
     char rev[len];
     strcpy(rev, str);
-    int i=0, j=len-1;
-    while(i<j)
+    int i = 0, j = len - 1;
+    while (i < j)
     {
         swap(rev[i], rev[j]);
         i++, j--;
@@ -41,7 +39,7 @@ void print_pattern(char *str)
             cout.fill('*');
         else
             cout.fill(' ');
-        cout.width(len);
+        // cout.width(i);
         cout.write(str, i);
         cout.width(len - i);
         cout << "";
--- practice codes/console/consolepractice.cpp
+++ practice codes/console/consolepractice.cpp
@@ -1,12 +1,54 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+void print(char str[])
+{
+    int len = strlen(str);
+    char rev[len];
+    strcpy(rev, str);
+    string s = "";
+    int i = 0, j = len - 1;
+    while (i < j)
+    {
+        swap(str[i], str[j]);
+        i++, j--;
+    }
+    for (int i = 0; i < strlen(rev); i++)
+    {
+        s = s + rev[i];
+    }
+    for (int i = len; i > 0; i--)
+    {
+        if (i == 1)
+            cout.fill('*');
+        else
+            cout.fill(' ');
+        cout.write(str, i);
+        cout.width(len - i);
+        cout << "";
+        cout.width(len - i);
+        cout << "";
+        cout << s.substr(len - i, len);
+        cout << endl;
+    }
+    // second part;
+    for (int i = 2; i <= len; i++)
+    {
+        cout.fill(' ');
+        cout.write(str, i);
+        cout.width(len - i);
+        cout << "";
+        cout.width(len - i);
+        cout << "";
+        cout << s.substr(len - i, len);
+        cout << endl;
+    }
+}
+
 int main()
 {
-    // fill
-    ostream & name(ostream & output)
-    {
-        output << "My name is: ";
-        return output;
-    }
-    cout << name << DEEPTEJ << endl;
+    char str[50];
+    cin >> str;
+    print(str);
 }
